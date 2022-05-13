1ce28c3a12001ad20985a3e79c3e6715692dd9d3
EsneiderCg <esneider20corgom@gmail.com>
2022-05-13 11:19:44 -0500

    proyecto_formativo

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles_proyecto.css">
+    <link rel="stylesheet" href="icons/all.css">
+    <title>Pagina Inicial</title>
+</head>
+<body>
+    <nav>
+        <img class="logo-principal" src="img/logo.png" alt="logo">
+        <ul class="menu-principal">
+            <li><a href=""> Registrar</a></li>
+            <li><a href="">ferta</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">transporte</a></li>
+        </ul>
+        <div class="buscar_lupa">
+            <input class="input-busqueda" type="text" name="id">
+            <i  class="icono_busqueda fa-solid fa-magnifying-glass"></i>
+        </div>
+    </nav>
+    <div class="main-title">
+        <h2>Un portal para vender y comprar productos Agricolas sin intermediarios y a mejores precios</h2>
+    </div>
+    <div class="main-contenedor">
+        <div class="producto-dia">
+            <div class="con-lulo"><img class="img-lulo" src="img/lulo994x782.png" alt="lulo"></div>
+            <div class="con-yuca"><img src="img/yuca1.024x788.jpg" alt="yuca" class="img-yuca"></div>
+            <div class="con-cafe"><img src="img/cafe_803x477.jpg" alt="cafe" class="img-cafe"></div>
+        </div>
+        <div class="lo-mas-vendido"><img src="img/racimo_platano861x_1390.jpg" alt="platano" class="img-platano"></div>
+    </div>
+</body>
+</html>
